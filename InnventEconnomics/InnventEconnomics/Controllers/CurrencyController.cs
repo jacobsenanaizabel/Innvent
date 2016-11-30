d951aded841b94aa58eca66682c33ab7d9317ad1
jacobsenanaizabel <ana.izabel@vtex.com.br>
2016-11-30 12:26:08 -0200

    refinamento na estrutura de apilayer

--- InnventEconnomics/InnventEconnomics/Controllers/CurrencyController.cs
+++ InnventEconnomics/InnventEconnomics/Controllers/CurrencyController.cs
@@ -4,19 +4,50 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 using InnventEconnomics.Model;
+using Newtonsoft.Json;
 
 namespace InnventEconnomics.Controllers
 {
     [RoutePrefix("api/currencies")]
     public class CurrencyController : ApiController
     {
-        private WebRequest webRequest;
+        public static string date_1;
+        public static string date_2;
+        public static string date_3;
+        public static string date_4;
+        public static string date_5;
+        public static string date_6;
+        public static string date_7;
+        public static string currency;
+
+
+        public  string url_1 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_1 + "&currencies=" +
+             currency + "&format=1";
+
+        public string url_2 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_2 + "&currencies=" +
+                     currency + "&format=1";
+
+        public string url_3 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_3 + "&currencies=" +
+                     currency + "&format=1";
+
+        public string url_4 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_4 + "&currencies=" +
+                     currency + "&format=1";
+
+        public string url_5 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_5 + "&currencies=" +
+                     currency + "&format=1";
+
+        public string url_6 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_6 + "&currencies=" +
+                     currency + "&format=1";
+
+        public string url_7 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_7 + "&currencies=" +
+                     currency + "&format=1";
 
         public HttpResponseMessage GetCurrencies()
         {
-            return null;
+            return Request.CreateResponse(HttpStatusCode.NotFound);
 
         }
          
@@ -28,36 +59,16 @@ namespace InnventEconnomics.Controllers
             {
                 CurrencyExchangeVo currencyExchange = new CurrencyExchangeVo();
                 currencyExchange.nome = currency;
-                string text = null;
+             
 
-                var date_1 = DateTime.Now.ToString("yyyy-mm-dd");
-                var date_2 = DateTime.Now.AddDays(-1).ToString("yyyy-mm-dd");
-                var date_3 = DateTime.Now.AddDays(-2).ToString("yyyy-mm-dd");
-                var date_4 = DateTime.Now.AddDays(-3).ToString("yyyy-mm-dd");
-                var date_5 = DateTime.Now.AddDays(-4).ToString("yyyy-mm-dd");
-                var date_6 = DateTime.Now.AddDays(-5).ToString("yyyy-mm-dd");
-                var date_7 = DateTime.Now.AddDays(-6).ToString("yyyy-mm-dd");
+                date_1 = DateTime.Now.ToString("yyyy-MM-dd");
+                date_2 = DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
+                date_3 = DateTime.Now.AddDays(-2).ToString("yyyy-MM-dd");
+                date_4 = DateTime.Now.AddDays(-3).ToString("yyyy-MM-dd");
+                date_5 = DateTime.Now.AddDays(-4).ToString("yyyy-MM-dd");
+                date_6 = DateTime.Now.AddDays(-5).ToString("yyyy-MM-dd");
+                date_7 = DateTime.Now.AddDays(-6).ToString("yyyy-MM-dd");
 
-                string url_1 = "http://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date"+date_1+"&currencies=" +
-                             currency+"&format=1";
-
-                string url_2 = "http://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_2 + "&currencies=" +
-                             currency + "&format=1";
-
-                string url_3 = "http://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_3 + "&currencies=" +
-                             currency + "&format=1";
-
-                string url_4 = "http://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_4 + "&currencies=" +
-                             currency + "&format=1";
-
-                string url_5 = "http://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_5 + "&currencies=" +
-                             currency + "&format=1";
-
-                string url_6 = "http://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_6 + "&currencies=" +
-                             currency + "&format=1";
-
-                string url_7 = "http://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date" + date_7 + "&currencies=" +
-                             currency + "&format=1";
 
         
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create("url_1");
@@ -67,8 +78,9 @@ namespace InnventEconnomics.Controllers
                 var response = (HttpWebResponse)request.GetResponse();
                     
 
-                using (var sr = new StreamReader(response.GetResponseStream()))
-                {
+                using (var sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {                  
+                    var text = JsonConvert.DeserializeObject<Object>(sr.ReadToEnd());
                     text = sr.ReadToEnd();
                    // currencyExchange.Dia1(sr);
                 }
@@ -82,7 +94,7 @@ namespace InnventEconnomics.Controllers
 
                 using (var sr = new StreamReader(response.GetResponseStream()))
                 {
-                    text = sr.ReadToEnd();
+                    var text = sr.ReadToEnd();
                     // currencyExchange.Dia1(sr);
                 }
 
@@ -96,7 +108,7 @@ namespace InnventEconnomics.Controllers
 
                 using (var sr = new StreamReader(response.GetResponseStream()))
                 {
-                    text = sr.ReadToEnd();
+                    var text = sr.ReadToEnd();
                     // currencyExchange.Dia1(sr);
                 }
 
@@ -110,7 +122,7 @@ namespace InnventEconnomics.Controllers
 
                 using (var sr = new StreamReader(response.GetResponseStream()))
                 {
-                    text = sr.ReadToEnd();
+                    var text = sr.ReadToEnd();
                     // currencyExchange.Dia1(sr);
                 }
 
@@ -123,7 +135,7 @@ namespace InnventEconnomics.Controllers
 
                 using (var sr = new StreamReader(response.GetResponseStream()))
                 {
-                    text = sr.ReadToEnd();
+                    var text = sr.ReadToEnd();
                     // currencyExchange.Dia1(sr);
                 }
 
@@ -137,7 +149,7 @@ namespace InnventEconnomics.Controllers
 
                 using (var sr = new StreamReader(response.GetResponseStream()))
                 {
-                    text = sr.ReadToEnd();
+                    var text = sr.ReadToEnd();
                     // currencyExchange.Dia1(sr);
                 }
 
@@ -149,10 +161,10 @@ namespace InnventEconnomics.Controllers
 
                 using (var sr = new StreamReader(response.GetResponseStream()))
                 {
-                    text = sr.ReadToEnd();
+                    var text = sr.ReadToEnd();
                     // currencyExchange.Dia1(sr);
                 }
-                return Request.CreateResponse(HttpStatusCode.OK, text);
+                return Request.CreateResponse(HttpStatusCode.OK, currencyExchange);
 
             }
             catch (Exception ex)
