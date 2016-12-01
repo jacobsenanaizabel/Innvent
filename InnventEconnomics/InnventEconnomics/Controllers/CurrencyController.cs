585ef25ebf0a39a8c764919ed39a60a880f64755
jacobsenanaizabel <ana.izabel@vtex.com.br>
2016-12-01 14:36:07 -0200

    solucao final para cotacao de 7 dias

--- InnventEconnomics/InnventEconnomics/Controllers/CurrencyController.cs
+++ InnventEconnomics/InnventEconnomics/Controllers/CurrencyController.cs
@@ -1,49 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Web.Http;
+using System.Web.Script.Serialization;
 using InnventEconnomics.Model;
-using Newtonsoft.Json;
 
 namespace InnventEconnomics.Controllers
 {
     [RoutePrefix("api/currencies")]
     public class CurrencyController : ApiController
     {
-        public static string date_1;
-        public static string date_2;
-        public static string date_3;
-        public static string date_4;
-        public static string date_5;
-        public static string date_6;
-        public static string date_7;
-        public static string currency;
-
-
-        public  string url_1 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date=" + date_1 + "&currencies=" +
-             currency + "&format=1";
-
-        public string url_2 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date=" + date_2 + "&currencies=" +
-                     currency + "&format=1";
-
-        public string url_3 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date=" + date_3 + "&currencies=" +
-                     currency + "&format=1";
-
-        public string url_4 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date=" + date_4 + "&currencies=" +
-                     currency + "&format=1";
-
-        public string url_5 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date=" + date_5 + "&currencies=" +
-                     currency + "&format=1";
-
-        public string url_6 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date=" + date_6 + "&currencies=" +
-                     currency + "&format=1";
-
-        public string url_7 = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date=" + date_7 + "&currencies=" +
-                     currency + "&format=1";
 
         public HttpResponseMessage GetCurrencies()
         {
@@ -59,113 +26,65 @@ namespace InnventEconnomics.Controllers
             {
                 CurrencyExchangeVo currencyExchange = new CurrencyExchangeVo();
                 currencyExchange.nome = currency;
-             
+                string[] datas = new string[7];
+                string[] urls = new string[7];
+                Object[] request = new ObjectContent[7];
+                datas[0] = DateTime.Now.ToString("yyyy-MM-dd");
 
-                date_1 = DateTime.Now.ToString("yyyy-MM-dd");
-                date_2 = DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
-                date_3 = DateTime.Now.AddDays(-2).ToString("yyyy-MM-dd");
-                date_4 = DateTime.Now.AddDays(-3).ToString("yyyy-MM-dd");
-                date_5 = DateTime.Now.AddDays(-4).ToString("yyyy-MM-dd");
-                date_6 = DateTime.Now.AddDays(-5).ToString("yyyy-MM-dd");
-                date_7 = DateTime.Now.AddDays(-6).ToString("yyyy-MM-dd");
-
-
-        
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("url_1");
-                request.Method = WebRequestMethods.Http.Get;
-                request.Accept = "application/json";
-
-                var response = (HttpWebResponse)request.GetResponse();
-                    
-
-                using (var sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
-                {                  
-                    var text = JsonConvert.DeserializeObject<Object>(sr.ReadToEnd());
-                    text = sr.ReadToEnd();
-                   // currencyExchange.Dia1(sr);
-                }
-
-                HttpWebRequest request2 = (HttpWebRequest)WebRequest.Create("url_1");
-                request.Method = WebRequestMethods.Http.Get;
-                request.Accept = "application/json";
-
-                var response2 = (HttpWebResponse)request.GetResponse();
-
-
-                using (var sr = new StreamReader(response.GetResponseStream()))
+                urls[0] = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date=" + datas[0] + "&currencies=" +
+             currency + "&format=1";
+                for (int i = 1; i < 7; i++)
                 {
-                    var text = sr.ReadToEnd();
-                    // currencyExchange.Dia1(sr);
+                    datas[i] = DateTime.Now.AddDays(-i).ToString("yyyy-MM-dd");
+                    urls[i] = "https://apilayer.net/api/live?access_key=bfbb3baa3a9fa924ef69d83cebb2111a&date=" + datas[i] + "&currencies=" +
+             currency + "&format=1";
                 }
 
-
-                HttpWebRequest request3 = (HttpWebRequest)WebRequest.Create("url_1");
-                request.Method = WebRequestMethods.Http.Get;
-                request.Accept = "application/json";
-
-                var response3 = (HttpWebResponse)request.GetResponse();
-
-
-                using (var sr = new StreamReader(response.GetResponseStream()))
+                for (int i = 1; i <= 7; i++)
                 {
-                    var text = sr.ReadToEnd();
-                    // currencyExchange.Dia1(sr);
+                    HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(urls[i]);
+                    httpRequest.Method = WebRequestMethods.Http.Get;
+                    httpRequest.Accept = "application/json";
+                    var response = (HttpWebResponse)httpRequest.GetResponse();
+
+                    using (var sr = new StreamReader(response.GetResponseStream()))
+                    {
+                        JavaScriptSerializer js = new JavaScriptSerializer();
+                   
+                        var text = sr.ReadToEnd();
+                        CurrencyExchangeVo currencyObj =
+                            (CurrencyExchangeVo) js.Deserialize(text, typeof(CurrencyExchangeVo));
+                        /*
+                        switch (i)
+                        {
+                            case 1:
+                                currencyExchange.Dia1 = currencyObj;
+
+                            case 2:
+                                currencyExchange.Dia2 = currencyObj;
+
+                            case 3:
+                                currencyExchange.Dia3 = currencyObj;
+
+                            case 4:
+                                currencyExchange.Dia4 = currencyObj;
+
+                            case 5:
+                                currencyExchange.Dia5 = currencyObj;
+
+                            case 6:
+                                currencyExchange.Dia6 = currencyObj;
+
+                            case 7:
+                                currencyExchange.Dia7 = currencyObj;
+
+                        }*/
+                    }
+
+
                 }
-
-
-                HttpWebRequest request4 = (HttpWebRequest)WebRequest.Create("url_1");
-                request.Method = WebRequestMethods.Http.Get;
-                request.Accept = "application/json";
-
-                var response4 = (HttpWebResponse)request.GetResponse();
-
-
-                using (var sr = new StreamReader(response.GetResponseStream()))
-                {
-                    var text = sr.ReadToEnd();
-                    // currencyExchange.Dia1(sr);
-                }
-
-                HttpWebRequest request5 = (HttpWebRequest)WebRequest.Create("url_1");
-                request.Method = WebRequestMethods.Http.Get;
-                request.Accept = "application/json";
-
-                var response5 = (HttpWebResponse)request.GetResponse();
-
-
-                using (var sr = new StreamReader(response.GetResponseStream()))
-                {
-                    var text = sr.ReadToEnd();
-                    // currencyExchange.Dia1(sr);
-                }
-
-
-                HttpWebRequest request6 = (HttpWebRequest)WebRequest.Create("url_1");
-                request.Method = WebRequestMethods.Http.Get;
-                request.Accept = "application/json";
-
-                var response6 = (HttpWebResponse)request.GetResponse();
-
-
-                using (var sr = new StreamReader(response.GetResponseStream()))
-                {
-                    var text = sr.ReadToEnd();
-                    // currencyExchange.Dia1(sr);
-                }
-
-                HttpWebRequest request7 = (HttpWebRequest)WebRequest.Create("url_1");
-                request.Method = WebRequestMethods.Http.Get;
-                request.Accept = "application/json";
-
-                var response7 = (HttpWebResponse)request.GetResponse();
-
-                using (var sr = new StreamReader(response.GetResponseStream()))
-                {
-                    var text = sr.ReadToEnd();
-                    // currencyExchange.Dia1(sr);
-                }
+                
                 return Request.CreateResponse(HttpStatusCode.OK, currencyExchange);
-
             }
             catch (Exception ex)
             {
